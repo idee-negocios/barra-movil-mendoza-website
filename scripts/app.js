4489724ae69621d2ac2491305f7d7a609d25af62
Alan <kiriost.s@gmail.com>
2017-01-06 20:21:36 -0300

    Set background to header and contact sections

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,25 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  $('#mensaje').focus(() => {
-    const el = $('#mensaje');
+  // elements
+  const messageEl = $('#mensaje');
+  const contactFormEl = $('#contact-form');
+
+  // submit form
+  contactFormEl.submit((e) => {
+    e.preventDefault();
+  });
+
+  // message textarea
+  messageEl.focus(() => {
+    const el = messageEl;
     if(el.val() === 'Mensaje') {
       el.val('');
     }
   });
 
   $('#mensaje').blur(() => {
-    const el = $('#mensaje');
+    const el = messageEl;
     if(el.val() === '') {
       el.val('Mensaje');
     }
