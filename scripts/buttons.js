aabea2809f124e2b54f48dfa7eb6c937971fd15d
Alan <kiriost.s@gmail.com>
2017-02-12 04:05:22 -0300

    Release

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,3 +1,25 @@
+function toggleButtons() {
+  $('.toggle-btn').click(function() {
+    let btn = $(this);
+    let dataToggle = btn.data('toggle');
+
+    $('#' + dataToggle).toggle('slow', function() {
+      let el = $(this);
+
+      let isShown = el.data('shown');
+      isShown = !isShown;
+      el.data('shown', isShown);
+
+      btn.empty();
+      if(isShown) {
+        btn.append($('<i class="fa fa-chevron-up"></i>'));
+      } else {
+        btn.append($('<i class="fa fa-chevron-down"></i>'));
+      }
+    });
+  });
+}
+
 export function init() {
   // Scroll from header to contact section
   $("#home-btn, #patio-cervecero-btn").click(function() {
@@ -5,4 +27,6 @@ export function init() {
       scrollTop: $("#contact").offset().top
     }, 'slow');
   });
+
+  toggleButtons();
 }
