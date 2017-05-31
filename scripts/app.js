4f4c879a50a0a281866194aab272d222d19ee5a2
Alan <alan.boglioli@gmail.com>
2017-05-31 19:38:43 -0300

    Show images in slide

--- scripts/app.js
+++ scripts/app.js
@@ -15,9 +15,31 @@ $(document).ready(() => {
   Footer.init();
 
   $('.slide').slick({
+    lazyLoad: 'ondemand',
     infinite: true,
-    slidesToShow: 1,
+    slidesToShow: 5,
     slidesToScroll: 1,
-    autoplay: true
+    autoplay: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
   });
 });
